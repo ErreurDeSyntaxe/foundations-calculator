719d8dc0ac91a7c962009c349fb8840e6ca5bd75
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2023-12-08 11:44:12 +0800

    Add +-*/ and operate functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,48 @@
-console.log("Hello World");
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    if (num2 == 0) 
+        return "這樣不對嘍";
+    return num1 / num2;
+}
+
+//This feature is not necessary
+function displayOperation(num1, operator, num2) {
+    let operation = String.toString(num1) + " " +
+        String.toString(operator) + " " +
+        String.toString(num2);
+}
+
+function operate(num1, operator, num2) {
+    let result = 0;
+    switch (operator) {
+        case "+":
+            result =  add(num1, num2);
+            break;
+        case "-":
+            result =  subtract(num1, num2);
+            break;
+        case "*":
+            result =  multiply(num1, num2);
+            break;
+        case "/":
+            result =  divide(num1, num2);
+            break;
+        default:
+            result = 0;
+            break;
+    }
+    //rounds the result to two decimals IF there was no divide by zero
+    if(typeof result == "number") result = Math.floor(result * 100) / 100;
+    return result;
+}
