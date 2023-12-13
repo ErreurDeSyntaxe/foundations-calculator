afbc81c07c7760fce46994fd2736896c3e58ee64
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2023-12-13 14:43:52 +0800

    Allow chain of /*-+ and clean code
    Multiple operations can be chained and be processed one at a time,
    meaning that (12 + 7 - 5 * 3 ) equals 42, not 4.
    
    The code had some ambitious functions in the work, but they were cut
    out because they were clearly not necessary. Perhaps also because they
    are out of my reach for now.

--- js/script.js
+++ js/script.js
@@ -21,13 +21,6 @@ function divide(num1, num2) {
         return +((num1 / num2).toFixed(6));
 }
 
-//This feature is not necessary
-function displayOperation(num1, operator, num2) {
-    let operation = String.toString(num1) + " " +
-        String.toString(operator) + " " +
-        String.toString(num2);
-}
-
 function operate(num1, operator, num2) {
     let result = 0;
     switch (operator) {
@@ -51,10 +44,6 @@ function operate(num1, operator, num2) {
     if (typeof result == "number")
         result = Math.floor(result * 10000000) / 10000000;
     return result;
-    // console.log(Math.floor(result * 10000000));
-    // console.log(result / 10000000);
-    // result = (Math.floor(result * 10000000) / 10000000).toFixed(6);
-    // return result;
 }
 
 function turnOffDecimal() {
@@ -89,7 +78,6 @@ function updateDisplay(string) {
         case "percent":
             display.textContent = ((display.textContent) / 100).toFixed(6);
             display.textContent = +display.textContent;
-            console.log(display.textContent);
             break;
         case ".":
             if (display.textContent == "+" ||
@@ -108,7 +96,15 @@ function updateDisplay(string) {
         case "*":
         case "/":
         case "-":
-            if (operator != "") break;
+            if (operator != "") {
+                secondTerm = display.textContent;
+                firstTerm = operate(+firstTerm, operator, +secondTerm);
+                secondTerm = "";
+                operator = string;
+                display.textContent = string;
+                turnOnDecimal();
+                break;
+            }
             firstTerm = display.textContent;
             display.textContent = string;
             operator = string;
@@ -164,37 +160,6 @@ function activateButtons() {
     functions[0].addEventListener("click", () => clearAll());
     functions[1].addEventListener("click", () => updateDisplay("negative"))
     functions[2].addEventListener("click", () => updateDisplay("percent"));
-}
-
-function setTheme() {
-    /* Earth Tones
-    Frame Background: #8c756a
-    Display Background: #7c6a65
-    Function Buttons: #a1887f
-    Number Buttons: #bcaaa4
-    Operation Buttons: #8c7b75
-
-    Ocean Vibes
-    Frame Background: #607d8b
-    Display Background: #546e7a
-    Function Buttons: #80cbc4
-    Number Buttons: #90a4ae
-    Operation Buttons: #78909c
-
-    Pastel Paradise
-    Frame Background: #c5e1a5
-    Display Background: #aed581
-    Function Buttons: #81c784
-    Number Buttons: #ffcc80
-    Operation Buttons: #90a4ae
-
-    Midnight Noir
-    Frame Background: #121212
-    Display Background: #333
-    Function Buttons: #2196f3 (Royal Blue)
-    Number Buttons: #ff9800 (Orange)
-    Operation Buttons: #ff4081 
-    */
 }
 
 activateButtons();
